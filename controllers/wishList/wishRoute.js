6d0e979b5f643e27111608d97679df58187ecd86
ayaneey <ayan.h18@hotmail.com>
2022-10-11 21:10:51 +0100

    restructured routes and tested tables in models

--- controllers/wishList/wishRoute.js
+++ controllers/wishList/wishRoute.js
@@ -1,22 +1,51 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, WishList, Product } = require("../../models");
 
-router.get('/', async (req, res)=> {try {
-    
-} catch (error) {
-    
-}})
-router.post('/', async (req, res) => {
-try {
-    
-} catch (error) {
-    
-}
-})
-router.delete('/', async (req, res) => {
+// if (req.session.loggedIn) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+// })};
+
+const path = require("path");
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./file.JSON"));
+});
+
+router.post('/', async (req, res)=> {
     try {
+      console.log(req.body);
+    const dbWishList = await WishList.create({
+      user_id: req.body.username,
+      product_id: req.body.product
+    });
+
+    // req.session.save(() => {
+    //   req.session.loggedIn = true;
+
+    //   res.status(200).json(dbWishList);
+    // });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+}})
+
+// router.get('/', async (req, res) => {
+// try {
+//     // get the user id from query params/req params
+ 
+//     //[array of product ids] = // get from WishList table where user_id = 
+
+
+// } catch (error) {
+//       console.log(err);
+//     res.status(500).json(err); 
+// }
+// })
+// router.delete('/', async (req, res) => {
+//     try {
         
-    } catch (error) {
+//     } catch (error) {
         
-    }
-})
+//     }
+// })
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const auth = require("./utils/auth");
+// const auth = require("./utils/auth");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
@@ -17,7 +17,25 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(routes);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
+

e35ca4c6136a6bd575559f0aaab77a3eb4e0c140
“SuleRas” <sm.ras@hotmailco.uk>
2022-10-17 22:21:09 +0100

    search js

--- public/js/search.js
+++ public/js/search.js
@@ -1,1 +1,24 @@
-const searchBar = document.getElementById("search");
+// get search bar element
+
+const searchInput = document.getElementById("searchInput");
+const button = document.getElementById("search");
+
+// store name elements in array-like object
+
+const namesFromDOM = document.getElementsByClassName("name");
+
+// listen for user events
+
+button.addEventListener("click", (event) => {
+  const search = searchInput.value;
+  console.log(searchInput.value);
+
+  // get user search input converted to lowercase
+
+  //post call to the /:id pass the searchInput.value
+  const response = await fetch("/:name", {
+    method: "POST",
+    body: JSON.stringify({ search }),
+    headers: { "Content-Type": "application/json" },
+  });
+});

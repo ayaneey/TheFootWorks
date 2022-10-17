04184b4d3a745c14e514e6548b82ad9545c357d9
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-10-17 21:09:05 +0100

    login script

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,25 @@
-const login = async (event) => {
-    // Stop the browser from submitting the form so we can do so with JavaScript
-    event.preventDefault();
-  
-    // Gather the data from the form elements on the page
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      // Send the e-mail and password to the server
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
+const loginForm = async (event) => {
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  event.preventDefault();
+
+  // Gather the data from the form elements on the page
+  const email = document.querySelector("#floatingInput").value.trim();
+  const password = document.querySelector("#floatingPassword").value.trim();
+
+  if (email && password) {
+    // Send the e-mail and password to the server
+    const response = await fetch("/user", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in");
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+  }
+};
+
+document.querySelector(".loginForm").addEventListener("submit", loginForm);
